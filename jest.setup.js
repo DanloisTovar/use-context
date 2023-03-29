937c08758c96c36a25f994ef6471f6b59fc073b6
Danlois Tovar <danlois.tovar@gmail.com>
2023-03-29 20:44:55 -0300

    add jest and testing-library for practice testing

---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+// En caso de necesitar la implementación del FetchAPI
+import 'whatwg-fetch'; // <-- yarn add whatwg-fetch// En caso de necesitar la implementación del FetchAPI
+import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
